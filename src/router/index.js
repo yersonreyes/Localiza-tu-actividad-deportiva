2cdedf6e844067a68b1e628483aca9581cabfe8c
Yerson Reyes <yerson.programador@gmail.com>
2022-07-15 15:53:19 -0400

    implemented logic login

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import Store from "@/store/";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "Home",
     component: HomeView,
+    meta: {
+      accessLevel: "public",
+    },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+    meta: {
+      accessLevel: "public",
+    },
   },
   {
     path: "/about",
@@ -18,6 +31,9 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    meta: {
+      accessLevel: "public",
+    },
   },
 ];
 
@@ -25,4 +41,18 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  const accessLevel = to.meta.accessLevel;
+  const activeLogin = Store.getters["session/activeLogin"];
+
+  if (accessLevel === "public") {
+    next();
+  } else if (accessLevel === "private") {
+    if (activeLogin) {
+      next();
+    } else {
+      next("/login");
+    }
+  }
+});
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import { sessionModule as session } from "./modules/sessionModule.js";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -8,5 +8,7 @@ export default new Vuex.Store({
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    session,
+  },
 });
